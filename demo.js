5c1358c34941b069d1a5b550d97f409cfeff8fd1
Joel Jacobson <joel@trustly.com>
2018-06-11 20:39:07 +0200

    Try Treant, a tree drawing lib

--- demo.js
+++ demo.js
@@ -16,5 +16,35 @@ Papa.parse('http://localhost/~joel/dregexp/node_types.csv', {
         let parseTree = drx.parse(nodeString);
         console.log(parseTree);
 
+        function createTreantNodeStructure(parseTree) {
+            let node = {
+                text: { name: parseTree[0] }
+            };
+            if (parseTree[1].constructor === Array) {
+                let children = [];
+                for (let child of parseTree[1]) {
+                    children.push(createTreantNodeStructure(child));
+                }
+                node.children = children;
+            } else {
+                node.children = [
+                    {text: { name: parseTree[1] }}
+                ];
+            }
+            return node;
+        }
+
+        let simple_chart_config = {
+            chart: {
+                container: "#OrganiseChart-simple",
+                node: {
+                    collapsable: true
+                }
+            },
+            nodeStructure: createTreantNodeStructure(parseTree)
+        };
+
+        new Treant( simple_chart_config );
+
     }
 });
