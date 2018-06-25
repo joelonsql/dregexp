3265907941f43a512492139b38577573b8da1e01
Joel Jacobson <joel@trustly.com>
2018-06-25 11:23:45 +0200

    Add new tool called "drep" allowing grepping on token type sequences instead of plain text
    Also add Rust keywords to rust.csv

---
+++ code-examples/for.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    for x in /* some comment */ 0..5 {
+        println!("zero to five: {}", x);
+    }
+
+    /* Some code example in a comment:
+       for y in 1..3 {
+          println("{}", y);
+       }
+    */
+
+    let mystr = r#"this looks like a for i in 1..100 { println("{}", i); } loop but is not one"#;
+    println!("{}", mystr);
+
+    for
+    z // some other comment
+    in 5..10 {
+        println!("{}", z);
+    }
+
+}
